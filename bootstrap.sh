1dbd5f95999a05e1a85520cca16939ad722048a7
lockan <lockan@gmail.com>
2018-12-01 13:49:08 +1300

    Bootstrap script fixes

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,13 +1,25 @@
+#!/bin/bash
+
 #Mount ebs volume
+#lsblk
+#sudo file -s /dev/xvdb
+#sudo mkfs -t ext4 /dev/xvdb
+#sudo mkdir /data
+#sudo mount /dev/xvdb /data
+
+#Set volume to auto-mount on restarts
+#sudo cp /etc/fstab /etc/fstab.bak
+#sudo echo '/dev/xvdb	/data	ext4	defaults,nofail' >> /etc/fstab
+#sudo mount -a
 
 # Update repo sources
-sudo yum update /y
+sudo yum update -y
 
 #Instal git
-sudo yum install git /y
+sudo yum install git -y
 
 # Install and start docker
-sudo amazon-linux-extras install docker /y
+sudo amazon-linux-extras install docker -y
 sudo service docker start
 sudo systemctl enable docker
 sudo usermod -aG docker ec2-user
